b6b8c98e511d060c0627ff5b8fe4edc25167e23a
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2024-10-03 22:37:21 +0300

    Update ContentView
    Updated ContentView

--- SoruPay/SoruPay/ContentView.swift
+++ SoruPay/SoruPay/ContentView.swift
@@ -5,21 +5,26 @@
 //  Created by Berke  on 1.10.2024.
 //
 
+// ContentView.swift
+
 import SwiftUI
 
-
 struct ContentView: View {
+    @EnvironmentObject var session: SessionStore
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            if session.currentUser != nil {
+                MainView()
+            } else {
+                LoginView()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(SessionStore())
+    }
 }
